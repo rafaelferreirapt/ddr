260c13028d7c88a7cfd763ccacc2ae4813851ab3
Rafael Ferreira <mail@rafaelferreira.pt>
2016-05-31 12:55:37 +0100

    refresh tables script to generate for all networks

---
+++ Assigment 4 - Network Traffic Engineering/code/refresh_tables.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -x
+N=${1:-5}
+
+source ../virtualenv/bin/activate
+for (( c=1; c<=${N}; c++ ))
+do
+    python NetTE${c}.py -f smallnet.dat
+    python NetTE${c}.py -f network.dat
+done
